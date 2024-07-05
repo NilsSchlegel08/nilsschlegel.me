36f9a196d0f04fe102fa978902017824d68b161e
Truthy <steamnilsoo@gmail.com>
2024-07-05 15:00:22 +0200

    Animations.tsx Syntax error fix

--- src/components/animation/Animations.tsx
+++ src/components/animation/Animations.tsx
@@ -1,33 +1,44 @@
 export function fadeout(id: string, time: number) {
-    let target: HTMLElement = document.getElementById(id);
+    const target = document.getElementById(id);
+    if (!target) return;
+    
     let step = 10;
     let opacityStep = step / time;
-
-    for (let i = 0; i <= time; i += step) {
-        setTimeout(() => {
-            let opacity = parseFloat(getComputedStyle(target).opacity);
-            if (opacity > 0) {
-                target.style.opacity = (opacity - opacityStep).toString();
-            } else {
-                target.style.display = 'none';
-            }
-        }, i);
+    let opacity = 1; 
+    
+    function decrease() {
+        opacity -= opacityStep;
+        if (opacity <= 0) {
+            (target as HTMLElement).style.opacity = '0';
+            (target as HTMLElement).style.display = 'none';
+        } else {
+            (target as HTMLElement).style.opacity = opacity.toString();
+            setTimeout(decrease, step);
+        }
     }
+
+    decrease();
 }
 
 export function fadein(id: string, time: number) {
-    let target: HTMLElement = document.getElementById(id);
-    target.style.display = 'block';
-    target.style.opacity = '0';
+    const target = document.getElementById(id);
+    if (!target) return;
+
+    (target as HTMLElement).style.display = 'block';
+    (target as HTMLElement).style.opacity = '0';
     let step = 10;
     let opacityStep = step / time;
-
-    for (let i = 0; i <= time; i += step) {
-        setTimeout(() => {
-            let opacity = parseFloat(getComputedStyle(target).opacity);
-            if (opacity < 1) {
-                target.style.opacity = (opacity + opacityStep).toString();
-            }
-        }, i);
+    let opacity = 0;
+    
+    function increase() {
+        opacity += opacityStep;
+        if (opacity >= 1) {
+            (target as HTMLElement).style.opacity = '1';
+        } else {
+            (target as HTMLElement).style.opacity = opacity.toString();
+            setTimeout(increase, step);
+        }
     }
+
+    increase();
 }
